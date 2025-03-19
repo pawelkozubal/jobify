59619dcd33324a66aa3451b95972e708f9ba18ae
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-03-19 23:16:57 +0000

    added Auth and links and some layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import {ClerkProvider} from '@clerk/nextjs';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -14,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
+    
   );
 }
