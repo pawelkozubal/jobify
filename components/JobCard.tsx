feec22e468280cf34c128deda55fd287e1c7a946
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-03-31 01:41:39 +0100

    half way of completingh the job card

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,6 +1,38 @@
 import { JobType } from '@/utils/types';
+import { MapPin, Briefcase, CalendarDays, RadioTower } from 'lucide-react';
+
+import Link from 'next/link';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { Badge } from './ui/badge';
+import JobInfo from './JobInfo';
+import DeleteJobButton from './DeleteJobButton';
 
 function JobCard({ job }: { job: JobType }) {
-  return <h1 className='text-3xl'>JobCard</h1>;
-}
-export default JobCard;
+    const date = new Date(job.createdAt).toLocaleDateString();
+    return (
+      <Card className='bg-muted'>
+        <CardHeader>
+          <CardTitle>{job.position}</CardTitle>
+          <CardDescription>{job.company}</CardDescription>
+        </CardHeader>
+        <Separator />
+        <CardContent>{/* card info */}</CardContent>
+        <CardFooter className='flex gap-4'>
+          <Button asChild size='sm'>
+            <Link href={`/jobs/${job.id}`}>edit</Link>
+          </Button>
+          <DeleteJobButton />
+        </CardFooter>
+      </Card>
+    );
+  }
+  export default JobCard;
